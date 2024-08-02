ea43dd4297fa41870e4eb817fa7923383537c873
chaitu1 <chaitu143@gmail.com>
2024-08-02 23:24:35 +0530

    Argocd Jobs

--- cart/main.tf
+++ cart/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+
 data "kubectl_file_documents" "cart" {
   content = file("pod.yaml")
 }
