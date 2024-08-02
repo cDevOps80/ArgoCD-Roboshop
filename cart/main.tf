ee6db2355fd464c3fae3a7b80c5ef13b139b7ebb
chaitu1 <chaitu143@gmail.com>
2024-08-02 23:28:29 +0530

    Argocd Jobs

--- cart/main.tf
+++ cart/main.tf
@@ -10,15 +10,24 @@ terraform {
 }
 
 
-data "kubectl_file_documents" "cart" {
-  content = file("pod.yaml")
-}
-
-#resource "kubectl_manifest" "test" {
-#  for_each  = data.kubectl_file_documents.cart.manifests
-#  yaml_body = each.value
+#data "kubectl_file_documents" "cart" {
+#  content = file("pod.yaml")
+#}
+#
+##resource "kubectl_manifest" "test" {
+##  for_each  = data.kubectl_file_documents.cart.manifests
+##  yaml_body = each.value
+##}
+#
+#resource "null_resource" "name" {
+#  count = length(data.kubectl_file_documents.cart.manifests)
 #}
 
-resource "null_resource" "name" {
-  count = length(data.kubectl_file_documents.cart.manifests)
+data "kubectl_filename_list" "manifests" {
+  pattern = "pod.yaml"
+}
+
+resource "kubectl_manifest" "test" {
+  count     = length(data.kubectl_filename_list.manifests.matches)
+  yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
 }
