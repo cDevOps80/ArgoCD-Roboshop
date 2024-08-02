d1aa416290344298540a86d753ecbe39d8846b13
chaitu1 <chaitu143@gmail.com>
2024-08-02 23:26:12 +0530

    Argocd Jobs

--- cart/main.tf
+++ cart/main.tf
@@ -14,7 +14,11 @@ data "kubectl_file_documents" "cart" {
   content = file("pod.yaml")
 }
 
-resource "kubectl_manifest" "test" {
-  for_each  = data.kubectl_file_documents.cart.manifests
-  yaml_body = each.value
+#resource "kubectl_manifest" "test" {
+#  for_each  = data.kubectl_file_documents.cart.manifests
+#  yaml_body = each.value
+#}
+
+resource "null_resource" "name" {
+  count = length(data.kubectl_file_documents.cart.manifests)
 }
