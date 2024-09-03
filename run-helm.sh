f3ace0e0e54e07a7d7c55e0b45994f287f78f0d8
chaithanya1812 <bob@example.com>
2024-09-03 10:03:25 +0530

    eks creation

--- run-helm.sh
+++ run-helm.sh
@@ -1,4 +1,4 @@
 
-[[ $2 = "" ]] && helm install $1 ./roboshop-helm -f dev-values/$1.yaml  && exit 0
-[[ $2 = "-d" ]] && helm upgrade -i $1 ./roboshop-helm -f dev-values/$1.yaml --dry-run && exit 0
+[[ $2 = "" ]] && helm upgrade -i $1 ./roboshop-helm -f dev-values/$1.yaml  && exit 0
+[[ $2 = "-d" ]] && helm install $1 ./roboshop-helm -f dev-values/$1.yaml --dry-run && exit 0
 
