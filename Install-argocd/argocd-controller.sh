666052bcd53c8ed52f1a0fb8030fdb5b20f1f9dc
chaithanya1812 <bob@example.com>
2024-09-12 19:49:05 +0530

    key pair

--- Install-argocd/argocd-controller.sh
+++ Install-argocd/argocd-controller.sh
@@ -5,5 +5,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 sleep 20
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-sleep 30
+sleep 10
 kubectl get secrets argocd-initial-admin-secret -o=jsonpath='{.data.password}' -n argocd  | base64 -d ; echo ""
